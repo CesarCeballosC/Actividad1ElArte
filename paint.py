ae4721d8e0a1341a7b8f284f8e8fe1e7c0dd1cfe
CesarCeballosC <105232610+CesarCeballosC@users.noreply.github.com>
2024-09-12 11:13:31 -0600

    Se añadiÃeron comentarios al código paint.py

--- paint.py
+++ paint.py
@@ -14,6 +14,7 @@ from turtle import *
 from freegames import vector
 
 
+# Se crea función para dibujar una linea desde un punto de inicio a un punto final
 def line(start, end):
     """Draw line from start to end."""
     up()
@@ -22,6 +23,7 @@ def line(start, end):
     goto(end.x, end.y)
 
 
+# Se crea función para dibujar un cuadrado desde un punto de inicio a un punto final
 def square(start, end):
     """Draw square from start to end."""
     up()
@@ -29,31 +31,33 @@ def square(start, end):
     down()
     begin_fill()
 
-    for count in range(4):
+    for count in range(4): # Se dibuja un cuadrado
         forward(end.x - start.x)
         left(90)
 
     end_fill()
 
 
+# Se crea función para dibujar un círculo con un tamaño designado por la persona
 def circlef(start, end):
     up()
-    goto(start.x, start.y)
+    goto(start.x, start.y) # Se mueve el cursor a la posición de inicio
     down()   
-    begin_fill()
+
+    begin_fill() # Se inicia a dibujar el círculo
     circle((end.x - start.x)/2)
     end_fill()
 
 
-
+# Se crea función para dibujar un rectángulo desde un punto de inicio a un punto final
 def rectangle(start, end):
     """Draw rectangle from start to end."""
     up()             
-    goto(start.x,start.y)         
+    goto(start.x,start.y) #Se mueve el cursor a la posición de inicio
     down()           
     begin_fill()        
 
-    for i in range(2):
+    for i in range(2): # Se dibuja un rectángulo
         forward(start.x)  
         left(90)
         forward(end.y) 
@@ -61,14 +65,15 @@ def rectangle(start, end):
 
     end_fill()  
 
+# Se crea función para dibujar un triángulo desde un punto de inicio a un punto final
 def triangle(start, end):
     """Draw triangle from start to end."""
 
-    up()
-    goto(start.x, start.y)
+    up() 
+    goto(start.x, start.y) # Se mueve el cursor a la posición de inicio
     down()
     
-    begin_fill()
+    begin_fill() # Se inicia a dibujar el triángulo
     x1, y1 = start.x, start.y
     x2, y2 = end.x, start.y
     x3, y3 = (start.x + end.x) / 2, end.y
@@ -83,33 +88,39 @@ def triangle(start, end):
 
 def tap(x, y):
     """Store starting point or draw shape."""
-    start = state['start']
+    start = state['start']  
 
     if start is None:
-        state['start'] = vector(x, y)
+        state['start'] = vector(x, y) # Se guarda el punto de inicio
     else:
-        shape = state['shape']
+        shape = state['shape'] # Se guarda la forma
         end = vector(x, y)
         shape(start, end)
         state['start'] = None
 
 
+
 def store(key, value):
     """Store value in state at key."""
-    state[key] = value
+    state[key] = value # Se guarda el valor en el estado de la llave
 
 
+# Se crea función para cambiar el color de la forma
 state = {'start': None, 'shape': line}
-setup(420, 420, 370, 0)
+setup(420, 420, 370, 0) # Se configura la ventana
 onscreenclick(tap)
 listen()
-onkey(undo, 'u')
+onkey(undo, 'u') # Se deshace la última acción
+
+# Se crean las teclas para cambiar el color de la forma
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('yellow'), 'Y')
+
+# Se crean las teclas para cambiar la forma de la figura
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circlef), 'c')
